b3450359363886f1fe4231e7a895bc12eb2e67ee
Marco Graf <grafmar78@gmail.com>
2026-03-09 11:24:56 +0100

    corrected build-batch-file to execute in build directory

--- src/build_scripts/build.bat
+++ src/build_scripts/build.bat
@@ -1,13 +1,12 @@
 @echo off
 
-cd /d %~dp0\..\pc_app
+cd /d %~dp0\.
 
 REM -------- Build OsciFootswitch --------
 REM 1. Version generieren
 python generate_version.py
 if ERRORLEVEL 1 (
     echo Fehler beim Generieren von version.txt
-    pause
     exit /b 1
 )
 
@@ -15,7 +14,6 @@ REM 2. PyInstaller Build
 pyinstaller OsciFootswitch.spec
 if ERRORLEVEL 1 (
     echo PyInstaller Build fehlgeschlagen
-    pause
     exit /b 1
 )
 
